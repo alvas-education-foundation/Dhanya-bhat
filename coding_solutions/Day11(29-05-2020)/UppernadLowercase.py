00cd9065e054edaf6609d4ef98f5a38ee247c9de
Dhanya Bhat <dhanyabhat29@gmail.com>
2020-05-29 18:23:18 +0530

    29-05-20

---
+++ coding_solutions/Day11(29-05-2020)/UppernadLowercase.py
@@ -0,0 +1,11 @@
+string=input("Enter a string : ")
+count1=0
+count2=0
+for i in string:
+    if (i.isupper()):
+        count1=count1+1
+    elif(i.islower()):
+        count2=count2+1
+print("Upper case characters :",count1)
+print("Lowe case characters : ",count2)
+
